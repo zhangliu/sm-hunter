dd8029e2d033c83d8fe09317a0ef56ba4554739d
zhangliu <zhangliuge@yeah.net>
2019-07-16 13:52:33 +0800

    modify

--- utils/nightmare/index.js
+++ utils/nightmare/index.js
@@ -1,12 +1,15 @@
 const Nightmare = require('nightmare')
 const querystring = require('querystring')
 const { sleep } = require('../time')
+const {runTimes} = require('../runHelper')
 
 const getNm = (opt = {}) => {
   const nightmare = Nightmare(opt)
   nightmare.zlWait = zlWait.bind(nightmare)
   nightmare.zlGoto = (url) => nightmare.goto(url).inject('js', `${__dirname}/helper.js`)
-  nightmare.zlClick = selector => zlClick.bind(nightmare, selector)
+  nightmare.zlClick = zlClick.bind(nightmare)
+  nightmare.zlTryClick = zlTryClick.bind(nightmare)
+  nightmare.zlLogin = zlLogin.bind(nightmare)
 
   nightmare.req = {
     get: get.bind(nightmare)
@@ -29,10 +32,47 @@ const zlWait = async function(selector, timeout = 20000) {
   throw new Error(`wait node "${selector}" time out!`)
 }
 
-const zlClick = selector => {
-  this.evaluate(selector => {
-    
+const zlClick = async function(selector, times) {
+  const fun = async () => await this.evaluate(selector => {
+    let ele
+    const match = selector.match(/(.*)\[innerText=(.*)\]$/)
+    if (match) {
+      const innerText = (match[2] || '').replace(/"/g, '')
+      ele = Array.from(document.querySelectorAll(match[1])).find(e => e.innerText === innerText)
+    } else ele = document.querySelector(selector)
+
+    if (!ele) throw new Error(`没有发现元素 ${selector}`)
+    ele.click()
   }, selector)
+
+  await runTimes(fun, times)
+}
+
+const zlTryClick = async function(selector, times){
+  try {
+    await zlClick.bind(this)(selector, times)
+  } catch(e) {
+    console.log(e)
+    return false
+  }
+}
+
+const zlLogin = async function(url, loginQuery, loginUrl) {
+  console.log(`尝试登录系统 ${url} 中`)
+  try {
+    return await this.goto(url).zlWait(loginQuery, 5000)
+  } catch(e) {
+    console.error(e)
+  }
+  console.log('发现状态为未登录，尝试登录中...')
+  if (loginUrl) await this.goto(loginUrl)
+
+  console.log('请输入用户名密码！')
+  const oldUrl = await this.url()
+  
+  await runTimes(async () => (await this.url() !== oldUrl), 90)
+
+  await this.zlLogin(url, loginQuery, loginUrl)
 }
 
 const get = function(url, opt = {}) {
--- utils/runHelper.js
+++ utils/runHelper.js
@@ -8,8 +8,8 @@ const runTimes = async (func, times = 5, timeout = 5000) => {
       return await runOnce(func, timeout)
     } catch (e) {
       console.log(e.message)
-      await sleep(1000)
     }
+    await sleep(1000)
   }
   throw new Error(`运行函数 ${times} 次失败！`)
 }
